09c225fab131d89bf75be2aafb8178aedb889f29
MiresPL <93918740+MiresPL@users.noreply.github.com>
2023-01-31 14:10:04 +0100

    sql cd

--- sql/kwerendy.sql
+++ sql/kwerendy.sql
@@ -1,1 +1,13 @@
 SELECT kraj, COUNT(*) AS "Liczba Osob" FROM gracze WHERE data_dolaczenia LIKE "2018%" GROUP BY kraj ORDER BY COUNT(*) DESC LIMIT 5;
+
+SELECT nazwa, SUM(strzal) FROM klasy WHERE nazwa LIKE "%elf%" GROUP BY nazwa;
+
+SELECT id_gracza
+FROM Gracze
+WHERE id_gracza NOT IN
+ 
+(SELECT id_gracza
+ 
+FROM Jednostki
+ 
+WHERE nazwa = "artylerzysta");
